657088a9a371bfd0b7a08844e82de33b9907c304
bo <61472065+bwd202@users.noreply.github.com>
2023-09-10 15:09:55 -0400

    save progress

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './style.css'
 import "@fontsource/urbanist";
+// import Icon from './assets/facebook.svg'
 
-console.log('it\'s alive!')
+let socialDiv = document.getElementById('socials')
+
+let facebook = new Image()
+
+// facebook.src = Icon
+
+// facebook.setAttribute('width', '100')
+// facebook.setAttribute('height', '100')
+
+socialDiv.appendChild(facebook)
