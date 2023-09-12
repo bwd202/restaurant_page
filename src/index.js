53a95ef37ecd2e5fb2042f3df29945eebf8c51e5
bo <61472065+bwd202@users.noreply.github.com>
2023-09-12 17:49:19 -0400

    save

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './style.css'
-import "@fontsource/urbanist";
+import "@fontsource/urbanist"
 import {loadPage} from './loadPage'
 import facebook from './assets/facebook.svg'
 import yelp from './assets/yelp.svg'
-import bbb from './assets/symbol.png'
+import bbb from './assets/bbb.svg'
 
 // ICONS
-let facebookLogo = new Image()
-let yelpLogo = new Image()
-let bbbLogo = new Image(50,50)
+let facebookLogo = new Image(50,50)
+let yelpLogo = new Image(50, 50)
+let bbbLogo = new Image(50, 50)
 
 facebookLogo.src = facebook
 yelpLogo.src = yelp
 bbbLogo.src = bbb
 
-document.querySelector('#socials p').append(facebookLogo, yelpLogo, bbbLogo)
-
 facebookLogo.classList.add('icon')
 yelpLogo.classList.add('icon')
-bbbLogo.classList.add('png')
+bbbLogo.classList.add('icon','scaled')
+
+document.querySelector('#socials p').append(facebookLogo, yelpLogo, bbbLogo)
 
 // BUTTONS
 let aboutBtn = document.querySelector('#navbar :last-child')
