e6b4493129e2e2a18c85c1245e014b137448bc14
bo <61472065+bwd202@users.noreply.github.com>
2023-09-13 12:08:24 -0400

    add dimensions for map in css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,34 @@ import facebook from './assets/facebook.svg'
 import yelp from './assets/yelp.svg'
 import bbb from './assets/bbb.svg'
 
+  // Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
+
 // ICONS
 let facebookLogo = new Image(30,30)
 let yelpLogo = new Image(30, 30)
