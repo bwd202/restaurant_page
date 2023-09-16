3743bbb7d5f5590f8645151e575f60d1cf83315d
bo <61472065+bwd202@users.noreply.github.com>
2023-09-16 16:05:28 -0400

    refactor pageLoad code
    add new fn createPage to aboutUs.js

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -1,6 +1,79 @@
-export {aboutUs}
+// export {aboutUs}
+export {createPage}
+import andy from './assets/andy-hay.jpg'
+import debbie from './assets/debbie-tea.jpg'
+import { LoremIpsum } from "lorem-ipsum";
+
+// Lorem Ipsum script
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
+// lorem.generateWords(1);
+// lorem.generateParagraphs(7);
+// lorem.generateSentences(5)
+
+// About Us pictures
+let andyPic = new Image(400,400)
+andyPic.src = andy
+
+let debbiePic = new Image(400, 400)
+debbiePic.src = debbie
 
 let aboutUs = {
     title: 'ABOUT US',
     content: '<p>' + 'Established in 2009, Pagoda brings together Middle Eastern, Indian and Chinese cuisines for an eclectic palate reminiscent of the cultural exchange that once took place along Central Asia\'s Silk Road.' + '</p>',
+}
+
+// HTML
+let title = document.createElement('h2')
+title.classList.add('title-font')
+
+let body = document.createElement('div')
+body.classList.add('txt-wrapper')
+
+let imgWrapper1 = document.createElement('div')
+let imgWrapper2 = document.createElement('div')
+
+imgWrapper1.classList.add('img-wrapper')
+imgWrapper2.classList.add('img-wrapper')
+
+// let container = document.querySelector('#modules')
+
+function createPage() {
+
+    let moduleWrap = new DocumentFragment()
+
+    title.innerHTML = aboutUs.title
+
+    body.insertAdjacentHTML('afterbegin', aboutUs.content)
+
+    body.insertAdjacentElement('beforeend', imgWrapper1)
+
+    imgWrapper1.insertAdjacentElement('beforeend', andyPic)
+
+    body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+    body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+    body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+
+    body.insertAdjacentElement('beforeend', imgWrapper2)
+    imgWrapper2.insertAdjacentElement('afterbegin', debbiePic)
+
+    body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+    body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+
+    // container.append(title, body)
+
+    moduleWrap.append(title, body)
+
+    return moduleWrap
+
 }
--- src/loadPage.js
+++ src/loadPage.js
@@ -1,62 +1,35 @@
 export {loadPage}
-import { aboutUs } from './aboutUs';
+// import { aboutUs } from './aboutUs';
+import {createPage} from './aboutUs'
 import {menu} from './menu'
-import andy from './assets/andy-hay.jpg'
-import debbie from './assets/debbie-tea.jpg'
+// import andy from './assets/andy-hay.jpg'
+// import debbie from './assets/debbie-tea.jpg'
 
-// LOREM IPSUM
-import { LoremIpsum } from "lorem-ipsum";
+// // about us module pictures
+// let andyPic = new Image(400,400)
+// andyPic.src = andy
 
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-// lorem.generateWords(1);
-// lorem.generateParagraphs(7);
-// lorem.generateSentences(5)
-
-// about us module pictures
-let andyPic = new Image(400,400)
-andyPic.src = andy
-
-let debbiePic = new Image(400, 400)
-debbiePic.src = debbie
+// let debbiePic = new Image(400, 400)
+// debbiePic.src = debbie
 
 // html loading module
-function loadPage(event = 'default') {
+function loadPage(event) {
 
     // console.log('test loadPage.js')
 
-    let title = document.createElement('h2')
-    title.classList.add('title-font')
+    // let title = document.createElement('h2')
+    // title.classList.add('title-font')
 
-    let body = document.createElement('div')
-    body.classList.add('txt-wrapper')
+    // let body = document.createElement('div')
+    // body.classList.add('txt-wrapper')
 
-    let imgWrapper1 = document.createElement('div')
-    let imgWrapper2 = document.createElement('div')
+    // let imgWrapper1 = document.createElement('div')
+    // let imgWrapper2 = document.createElement('div')
 
-    imgWrapper1.classList.add('img-wrapper')
-    imgWrapper2.classList.add('img-wrapper')
+    // imgWrapper1.classList.add('img-wrapper')
+    // imgWrapper2.classList.add('img-wrapper')
 
     let container = document.querySelector('#modules')
-
-    // default
-    if(event === 'default')
-    title.textContent = menu.title
-    body.textContent = menu.content
-
-    container.append(title, body)
-
-    // let cond = 'default'
-
 
     if(container.hasChildNodes()) {
 
@@ -64,28 +37,31 @@ function loadPage(event = 'default') {
     }
 
     let module = event.target.textContent
+    
+    // about us page
 
     if(module === 'About Us') {
 
-        title.innerHTML = aboutUs.title
+        // title.innerHTML = aboutUs.title
 
-        body.insertAdjacentHTML('afterbegin', aboutUs.content)
+        // body.insertAdjacentHTML('afterbegin', aboutUs.content)
 
-        body.insertAdjacentElement('beforeend', imgWrapper1)
+        // body.insertAdjacentElement('beforeend', imgWrapper1)
 
-        imgWrapper1.insertAdjacentElement('beforeend', andyPic)
+        // imgWrapper1.insertAdjacentElement('beforeend', andyPic)
 
-        body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
-        body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
-        body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+        // body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+        // body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+        // body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
 
-        body.insertAdjacentElement('beforeend', imgWrapper2)
-        imgWrapper2.insertAdjacentElement('afterbegin', debbiePic)
+        // body.insertAdjacentElement('beforeend', imgWrapper2)
+        // imgWrapper2.insertAdjacentElement('afterbegin', debbiePic)
 
-        body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
-        body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+        // body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
+        // body.insertAdjacentHTML('beforeend', '<p>' + lorem.generateParagraphs(1) + '</p>')
 
-        container.append(title, body)
+        // container.append(title, body)
+        container.append(createPage())
 
     } else if (module === 'Menu') {
 
