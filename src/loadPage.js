e83afaa195fd78513b23c78f5b0d8e37a395049a
bo <61472065+bwd202@users.noreply.github.com>
2023-09-16 16:22:07 -0400

    refactor pageLoad and menu page code

--- src/loadPage.js
+++ src/loadPage.js
@@ -1,32 +1,11 @@
 export {loadPage}
 import { aboutUs } from './aboutUs';
 import {menu} from './menu'
-// import andy from './assets/andy-hay.jpg'
-// import debbie from './assets/debbie-tea.jpg'
 
-// // about us module pictures
-// let andyPic = new Image(400,400)
-// andyPic.src = andy
-
-// let debbiePic = new Image(400, 400)
-// debbiePic.src = debbie
-
-// html loading module
+// loads html modules into index.html via js
 function loadPage(event) {
 
     // console.log('test loadPage.js')
-
-    // let title = document.createElement('h2')
-    // title.classList.add('title-font')
-
-    // let body = document.createElement('div')
-    // body.classList.add('txt-wrapper')
-
-    // let imgWrapper1 = document.createElement('div')
-    // let imgWrapper2 = document.createElement('div')
-
-    // imgWrapper1.classList.add('img-wrapper')
-    // imgWrapper2.classList.add('img-wrapper')
 
     let container = document.querySelector('#modules')
 
@@ -45,14 +24,7 @@ function loadPage(event) {
 
     } else if (module === 'Menu') {
 
-        // let menu = menuLoader()
-
-        // console.log(menu)
-
-        title.textContent = menu.title
-        body.textContent = menu.content
-
-        container.append(title, body)
+        container.append(menu())
     }
 
 }
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,27 @@
 export {menu}
 
-// function menuLoader() {
-//     return {menu}
-// }
-
-let menu = {
-    title: 'Menu',
-    content: 'TBD',
+let pageContent = {
+    heading: 'Menu',
+    text: 'TBD',
 }
+
+// HTML
+
+let title = document.createElement('h2')
+title.classList.add('title-font')
+
+let body = document.createElement('div')
+body.classList.add('txt-wrapper')
+
+function menu() {
+    let wrapper = new DocumentFragment()
+
+    title.innerHTML = pageContent.heading
+    body.innerHTML = pageContent.text
+
+    wrapper.append(title, body)
+
+    return wrapper
+}
+
+
